86f0405c3e81570d69856642dbf23fd65e98f90f
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2022-02-02 22:44:28 +0700

    Initial Commit

---
+++ iOS15-template/iOS15_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS15_templateApp.swift
+//  iOS15-template
+//
+//  Created by admin on 2/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS15_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
