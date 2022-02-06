26da77bf18e77a0b579f40dbdaccd3a54fe3b763
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-06 21:16:59 +0700

    Adding SF Symbols 3

--- iOS15-template/AccountView.swift
+++ iOS15-template/AccountView.swift
@@ -11,10 +11,31 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                VStack {
+                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3)) //customize colors
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(Image(systemName: "hexagon").symbolVariant(.fill)
+                                        .foregroundColor(.blue)
+                                        .font(.system(size: 200))
+                                    .offset(x: -50, y: -100))
+                    Text("The GIZzz")
+                        .font(.title.weight(.semibold))
+                    Image(systemName: "location")
+                        .imageScale(.large)
+                    Text("Thailand")
+                        .foregroundColor(.secondary)
+                }
+                .frame(maxWidth: .infinity)
+                        
                 Section {
-                    Text("Setting")
-                    Text("Billing")
-                    Text("Help")
+                    Label("Setting", systemImage: "gear")
+                    Label("Billing", systemImage: "creditcard")
+                    Label("Help", systemImage: "questionmark")
                 }
                 .listRowSeparatorTint(.blue) // seperator cplor
     //                .listRowSeparator(.hidden)
