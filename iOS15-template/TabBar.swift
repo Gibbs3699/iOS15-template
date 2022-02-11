34e826b0ce252f10113bf6e69b9339ac51c1aacb
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-11 23:28:54 +0700

    Adding GeometryReader and PreferenceKey

--- iOS15-template/TabBar.swift
+++ iOS15-template/TabBar.swift
@@ -10,21 +10,22 @@ import SwiftUI
 struct TabBar: View {
     @State var selectedTab: Tab = .home
     @State var color: Color = .teal
+    @State var tabItemWidth: CGFloat = 0
     
     var body: some View {
         ZStack(alignment: .bottom) {
-//            if selectedTab == .home {
-//                ContentView()
-//                    .frame(maxWidth: .infinity, maxHeight: .infinity) //to send tabbar to bottom
-////                    .offset(y: 300)
-//            } else if selectedTab == .explore {
-//                AccountView()
-//
-//            }
-            
+            //            if selectedTab == .home {
+            //                ContentView()
+            //                    .frame(maxWidth: .infinity, maxHeight: .infinity) //to send tabbar to bottom
+            ////                    .offset(y: 300)
+            //            } else if selectedTab == .explore {
+            //                AccountView()
+            //
+            //            }
+                        
             // used group to set modifier, group will inherite the modifier
             Group {
-                //switch is more cleaner than if else
+                //switch is more cleaner than if else, use with enum
                 switch selectedTab {
                 case .home:
                     ContentView()
@@ -36,87 +37,108 @@ struct TabBar: View {
                     AccountView()
                 }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity) //to send tabbar to bottom
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             
             HStack {
-                Spacer()
-                ForEach(tabItems) { item in
-                    Button {
-                        withAnimation(.spring(response: 0.3, dampingFraction: 0.5)) {
-                            selectedTab = item.tab
-                            color = item.color
-                        }
-                    } label: {
-                        VStack {
-                            Image(systemName: item.icon)
-                                .symbolVariant(.fill) //custom
-                                .font(.body.bold())
-                                .frame(width: 44,height: 29)
-                            Text(item.text)
-                                .font(.caption2) //small
-                                .lineLimit(1)
-                        }
-                        .frame(maxWidth: .infinity)
-                    } // to make space equally
-                    .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
-                    .blendMode(selectedTab == item.tab ? .overlay : .normal)
-                }
-                Spacer()
+                buttons
             }
             .padding(.horizontal, 8)
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
-            .background(.ultraThinMaterial, in:
-                RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
             .background(
-                HStack {
-                    if selectedTab == .library {Spacer()}
-                    if selectedTab == .explore {Spacer()}
-                    if selectedTab == .notifications {
-                        Spacer()
-                        Spacer()
-                    }
-                    Circle().fill(color).frame(width: 88)
-                    if selectedTab == .home {Spacer()}
-                    if selectedTab == .explore {
-                        Spacer()
-                        Spacer()
-                    }
-                    if selectedTab == .notifications {Spacer()}
-                }.padding(.horizontal,8)
+                background
             )
             .overlay(
-                HStack {
-                    if selectedTab == .library {Spacer()}
-                    if selectedTab == .explore {Spacer()}
-                    if selectedTab == .notifications {
-                        Spacer()
-                        Spacer()
-                    }
-                    Rectangle().fill(color)
-                        .frame(width: 28, height: 5)
-                        .cornerRadius(3)
-                        .frame(width: 88)
-                        .frame(maxHeight: .infinity, alignment: .top)
-                    if selectedTab == .home {Spacer()}
-                    if selectedTab == .explore {
-                        Spacer()
-                        Spacer()
-                    }
-                    if selectedTab == .notifications {Spacer()}
-
-                }
-                    .padding(.horizontal,18)
+                overlay
             )
-            .strokeStyle(cornerRadius: 34) //to blur
+            .strokeStyle(cornerRadius: 34)
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea()
         }
     }
+    
+    var buttons: some View {
+        ForEach(tabItems) { item in
+            Button {
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                    selectedTab = item.tab
+                    color = item.color
+                }
+            } label: {
+                VStack(spacing: 0) {
+                    Image(systemName: item.icon)
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 44, height: 29)
+                    Text(item.text)
+                        .font(.caption2)
+                        .lineLimit(1)
+                }
+                .frame(maxWidth: .infinity) // to make space equally
+            }
+            .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
+            .blendMode(selectedTab == item.tab ? .overlay : .normal)
+            .overlay(
+                GeometryReader { proxy in
+//                            Text("\(proxy.size.width)")
+                    Color.clear.preference(key: TabPreferenceKey.self, value: proxy.size.width)
+                }
+            )
+            .onPreferenceChange(TabPreferenceKey.self) { value in
+                tabItemWidth = value
+            }
+        }
+    }
+    
+    var background: some View {
+        HStack {
+            if selectedTab == .library { Spacer() }
+            if selectedTab == .explore { Spacer() }
+            if selectedTab == .notifications {
+                Spacer()
+                Spacer()
+            }
+            Circle().fill(color).frame(width: tabItemWidth)
+            if selectedTab == .home { Spacer() }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notifications { Spacer() }
+        }
+        .padding(.horizontal, 8)
+    }
+    
+    var overlay: some View {
+        HStack {
+            if selectedTab == .library { Spacer() }
+            if selectedTab == .explore { Spacer() }
+            if selectedTab == .notifications {
+                Spacer()
+                Spacer()
+            }
+            Rectangle()
+                .fill(color)
+                .frame(width: 28, height: 5)
+                .cornerRadius(3)
+                .frame(width: tabItemWidth)
+                .frame(maxHeight: .infinity, alignment: .top)
+            if selectedTab == .home { Spacer() }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notifications { Spacer() }
+        }
+        .padding(.horizontal, 8)
+    }
 }
+
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
         TabBar()
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
